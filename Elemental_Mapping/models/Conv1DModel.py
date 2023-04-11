6480fd6b487712ac43ea0299959934dd3703e315
igeor <ioannis.georvasilis@gmail.com>
2023-04-11 21:06:11 +0300

    Refactored model for automataically construction

--- Elemental_Mapping/models/Conv1DModel.py
+++ Elemental_Mapping/models/Conv1DModel.py
@@ -7,24 +7,43 @@ class Conv1DModel(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.hidden_dims = hidden_dims
-        self.conv1 = nn.Conv1d(1, 64, 5, stride=2, padding=2)
-        self.conv2 = nn.Conv1d(64, 64, 3, stride=2, padding=2)
         self.relu = nn.ReLU()
         self.drop = nn.Dropout(0.1)
         self.maxpool = nn.MaxPool1d(2)
-        self.fcn = nn.Sequential( nn.Linear(8192, 512), nn.ReLU(), nn.Linear(512, self.out_features), nn.ReLU() )
+        self.state = 'test'
+        
+        # Initialize a Modulelist with len(hidden_dims) layers
+        self.conv_layers = nn.ModuleList([nn.Conv1d(1, self.hidden_dims[0], 5, stride=2, padding=2)])
+        # Add the rest of the layers
+        for i in range(len(self.hidden_dims)-1):
+            self.conv_layers.append(nn.Conv1d(self.hidden_dims[i], self.hidden_dims[i+1], 3, stride=2, padding=2))
+       
+        # Initialize the fully connected layer
+        self.num_flatten = self.hidden_dims[-1] * (in_features // (len(self.conv_layers) ** 4))
+        self.l1 = nn.Linear(self.num_flatten, self.num_flatten//2)
+        self.l2 = nn.Linear(self.num_flatten//2, self.out_features)
         
     def set_state(self, state):
-        if state in ['train', 'eval', 'test']:
+        if self.state in ['train', 'eval', 'test']:
             self.state = state
 
     def forward(self, x):
-        x = self.maxpool(self.relu(self.conv1(x)))
-        if self.state == 'train': x = self.drop(x)
-        x = self.maxpool(self.relu(self.conv2(x)))
-        if self.state == 'train': x = self.drop(x)
+        # Forward pass through the convolutional layers
+        for i, conv in enumerate(self.conv_layers):
+            if self.state == 'train':
+                x = self.maxpool(self.drop(self.relu(conv(x))))
+            else:
+                x = self.maxpool(self.relu(conv(x)))
         x = torch.flatten(x, start_dim=1) 
-        return self.fcn(x)
+        
+        if self.state == 'train':
+            x = self.drop(self.relu(self.l1(x)))
+            x = self.relu(self.l2(x))
+        else:
+            x = self.relu(self.l1(x))
+            x = self.relu(self.l2(x))
+            
+        return x
 
     def train(self, dataloader, optimizer, criterion, epochs=1000, 
               device='cuda', experiment_alias='default'):
@@ -63,6 +82,8 @@ class Conv1DModel(nn.Module):
 if __name__ == "__main__":
     device = 'cuda'
     x = torch.rand(32, 1, 2048).to(device)
-    model = Conv1DModel(out_features=1, device='cuda')
-    print(model(x).shape)
-    print(sum(p.numel() for p in model.parameters()))
+    model = Conv1DModel(hidden_dims=[64,64,64,128], out_features=1).to(device)
+    
+    with torch.no_grad():
+        y = model(x)
+    
